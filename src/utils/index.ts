ae99c8e542a69d17abc2fcd697a7471144c9bce4
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2023-08-03 22:27:07 +0530

    add downlaod image helper function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,13 @@
 //generate and return random prompt
 import { surpriseMePrompts } from "../constants";
+import FileSaver from 'file-saver';
 
 export const getRandomPrompt:any=(promt:string)=>{
     const randomIndex = Math.floor(Math.random()*surpriseMePrompts.length);
     const randomPromt = surpriseMePrompts[randomIndex];
     return promt === randomPromt ? getRandomPrompt(promt):randomPromt
+}
+
+export const downloadImage = async (id:any, photo:any) => {
+    FileSaver.saveAs(photo,`download-${id}.jpg`)
 }
